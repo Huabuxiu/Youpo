404d4c27dc19ae56bd3b06e45cd2c51c6db039ca
乾月 <myw227980@alibaba-inc.com>
2021-11-08 00:34:53 +0800

    删除节点

--- datastruct/skipList.go
+++ datastruct/skipList.go
@@ -89,6 +89,16 @@ func (skipList *SkipList) Length() int64 {
 	return skipList.length
 }
 
+func (skipList *SkipList) ForEach(function define.FunctionInterface) {
+	curNode := skipList.head
+
+	for curNode != nil {
+		function(curNode)
+		curNode = curNode.level[0].nextNode
+	}
+
+}
+
 func (skipList *SkipList) Insert(score float64, value interface{}) {
 	skipList.check()
 
@@ -121,6 +131,7 @@ func (skipList *SkipList) Insert(score float64, value interface{}) {
 		//前一个节点的next 为 nil
 		if preNodeItem.level[i].nextNode == nil {
 			preNodeItem.level[i].nextNode = node
+			//当前层的前驱节点到当前层的距离 等于 第0层前驱节点的值+1 - 当前层前驱节点的Rank
 			preToCurSpan := preNodeRank[0] + 1 - preNodeRank[i]
 
 			//当前节点到nil 的span 是原来 前置节点到 next 的span -  原来 前置节点到 当前节点的span
@@ -131,25 +142,82 @@ func (skipList *SkipList) Insert(score float64, value interface{}) {
 			node.level[i].nextNode = preNodeItem.level[i].nextNode
 			preNodeItem.level[i].nextNode = node
 
-			preToNilSpan := preNodeItem.level[i].span
+			preToNextSpan := preNodeItem.level[i].span
 			preToCurSpan := preNodeRank[0] + 1 - preNodeRank[i]
 
 			//当前节点到nil 的span 是原来 前置节点到 next 的span -  原来 前置节点到 当前节点的span
-			node.level[i].span = preToNilSpan - preToCurSpan
+			node.level[i].span = preToNextSpan - preToCurSpan
 			preNodeItem.level[i].span = preToCurSpan
 		}
 	}
 	node.backWard = preNode[0]
+
+	if skipList.tail == nil || node.backWard == skipList.tail {
+		skipList.tail = node
+	}
+
 	skipList.length++
 
 }
 
 func (skipList *SkipList) Remove(score float64, value interface{}) {
+	skipList.check()
 
+	node := skipList.getNode(score, value)
+	if node == nil {
+		return
+	}
+
+	//找到每层的前驱节点
+	preNode := make([]*SkipListNode, len(node.level))
+
+	skipList.removeNode(node, preNode)
+}
+
+func (skipList *SkipList) removeNode(node *SkipListNode, preNode []*SkipListNode) {
+	for i := len(node.level) - 1; i >= 0; i-- {
+		currNode := skipList.head
+
+		for currNode.level[i].nextNode.data != node.data {
+			currNode = currNode.level[i].nextNode
+		}
+
+		preNode[i] = currNode
+	}
+
+	for i := len(node.level) - 1; i >= 0; i-- {
+		preNode[i].level[i].nextNode = node.level[i].nextNode
+		preNode[i].level[i].span += node.level[i].span - 1
+	}
+
+	//尾节点
+	if node.level[0].nextNode == nil {
+		skipList.tail = node.backWard
+	} else {
+		node.level[0].nextNode.backWard = preNode[0]
+	}
+
+	//如果删除了最高层的节点
+	if len(node.level) == skipList.maxLevel {
+		for skipList.head.level[skipList.maxLevel].nextNode == nil {
+			skipList.maxLevel--
+		}
+	}
+
+	skipList.length--
 }
 
 func (skipList *SkipList) GetRank(score float64, value interface{}) int64 {
 	skipList.check()
+	rank, node := skipList.findNodeAndGetRank(score, value)
+	if node != nil && rank != 0 {
+		return rank
+	}
+
+	return 0
+}
+
+func (skipList *SkipList) findNodeAndGetRank(score float64, value interface{}) (int64, *SkipListNode) {
 	var rank int64 = 0
 
 	currNode := skipList.head
@@ -165,25 +233,24 @@ func (skipList *SkipList) GetRank(score float64, value interface{}) int64 {
 		}
 
 		if currNode.data.value == value {
-			return rank
+			return rank, currNode
 		}
 
 	}
-	return 0
+	return 0, nil
 }
 
-func (skipList *SkipList) ForEach(function define.FunctionInterface) {
-	curNode := skipList.head
-
-	for curNode != nil {
-		function(curNode)
-		curNode = curNode.level[0].nextNode
+func (skipList *SkipList) getNode(score float64, value interface{}) *SkipListNode {
+	skipList.check()
+	rank, node := skipList.findNodeAndGetRank(score, value)
+	if node != nil && rank != 0 {
+		return node
 	}
-
+	return nil
 }
 
-func (skipList *SkipList) GetByRank(rank int64) (float64, interface{}) {
-	return 0, 1
+func (skipList *SkipList) GetByRank(rank int64) *SkipListNode {
+	return nil
 }
 
 func (skipList *SkipList) HasInRange(start float64, end float64) bool {
--- datastruct/skipList_test.go
+++ datastruct/skipList_test.go
@@ -6,19 +6,6 @@ import (
 	"testing"
 )
 
-func TestSkipList_Insert(t *testing.T) {
-	skipList := MakeSkipList()
-
-	for i := 0.0; i < 100.0; i += 1.0 {
-		skipList.Insert(i, strconv.FormatFloat(i/10.0, 'f', 1, 64))
-	}
-
-	rank := skipList.GetRank(6.0, "0.6")
-	println(rank)
-
-	printSkipList(skipList)
-}
-
 func printSkipList(skipList *SkipList) {
 	skipList.ForEach(func(node interface{}) interface{} {
 		print("score : ", fmt.Sprint(node.(*SkipListNode).data.score), " value :", fmt.Sprint(node.(*SkipListNode).data.value))
@@ -34,5 +21,31 @@ func printSkipList(skipList *SkipList) {
 		println(lStr)
 		return node
 	})
-	println("maxLevel", skipList.maxLevel)
+	println("maxLevel", skipList.maxLevel, "length :", skipList.Length())
+}
+
+func TestSkipList_Insert(t *testing.T) {
+	skipList := MakeSkipList()
+
+	for i := 0.0; i < 100.0; i += 1.0 {
+		skipList.Insert(i, strconv.FormatFloat(i/10.0, 'f', 1, 64))
+	}
+
+	rank := skipList.GetRank(6.0, "0.6")
+	println(rank)
+
+	printSkipList(skipList)
+}
+
+func TestSkipList_Remove(t *testing.T) {
+	skipList := MakeSkipList()
+
+	for i := 0.0; i < 100.0; i += 1.0 {
+		skipList.Insert(i, strconv.FormatFloat(i/10.0, 'f', 1, 64))
+	}
+	printSkipList(skipList)
+
+	skipList.Remove(87.0, "8.7")
+
+	printSkipList(skipList)
 }
